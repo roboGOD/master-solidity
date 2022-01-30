55800824f69f26e993485c6b1c3e2cb791253e00
roboGOD <ishwer.kumar666@gmail.com>
2022-01-30 11:00:07 +0530

    arrays

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -23,4 +23,17 @@ contract MyContract {
     // string memory _myLocalString = "Hello, Function!";
     return value;
   }
+
+  // Array
+  uint[] public uintArray = [1, 2, 3];
+  string[] public stringArray = ["apple"];
+  uint[][] public array2D = [ [1, 2, 3], [4, 5, 6]];
+
+  function addValue(string memory _value) public {
+    stringArray.push(_value);
+  }
+
+  function valueCount() public view returns (uint) {
+    return stringArray.length;
+  }
 }
--- src/console/MyContract.js
+++ src/console/MyContract.js
@@ -1,3 +1,4 @@
+// Data Types
 let myContract = await MyContract.deployed();
 let myLocalVal = (await myContract.getValue()).toNumber();
 let myUint = (await myContract.myUint()).toNumber();
@@ -6,3 +7,13 @@ let myString = (await myContract.myString());
 let myStruct = (await myContract.myStruct());
 console.log(myStruct.myInt.toNumber());
 console.log(myStruct.myString);
+
+// Arrays
+(await myContract.uintArray(0)).toNumber();
+await myContract.stringArray(0);
+(await myContract.array2D(1, 0)).toNumber();
+
+await myContract.addValue('orange');
+let strArrayLen = (await myContract.valueCount()).toNumber();
+// read last element of array
+await myContract.stringArray(strArrayLen-1);
